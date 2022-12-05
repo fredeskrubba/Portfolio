f5f12b38fb8161dae8d4e77d23fc6c51d7a163f5
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-12-05 09:30:32 +0100

    fix

--- src/components/header/header-banner/Banner.js
+++ src/components/header/header-banner/Banner.js
@@ -16,11 +16,11 @@ const Banner = () => {
       <div className='banner-wrapper'>
         <div className='banner-transition'>
           <div className='banner-grid'>
-            {bubbles.map(bubble => {
-              return <BannerItem text={bubble} className="banner-item"/>
-            })}
+                {bubbles.map(bubble => {
+                  return <BannerItem text={bubble} className="banner-item"/>
+                })}
+              </div>
           </div>
-        </div>
       </div>
     </div>
   )

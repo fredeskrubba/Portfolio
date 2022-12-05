12fbc3b673ff185287acfb364c8aac0b978137bf
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-12-05 10:17:31 +0100

    made about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/header/Header'
+import About from '../components/About'
+import profile from '../assets/img/profile.jpg'
+
 const Home = () => {
   return (
     <>
       <Navbar/>
       <Header/>
+      <About img={profile}/>
     </>
   )
 }

d485e35f89e227d3a3449eab66eb7e71db5b1330
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2022-12-13 21:13:44 +0100

    reworked again

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../styles/homepage/header.css'
-import headerImage from '../../assets/img/header-image.jpg'
+
 
 const Header = () => {
   return (
@@ -8,11 +8,6 @@ const Header = () => {
       <section className='upper-header'>
         <h1 className=''>Frederik Skrubbeltrang</h1>
         <div className='seperator-1'></div>
-      </section>
-      <section className='lower-header'>
-        <img src={headerImage} alt="header-image" />
-        <div className='seperator-2'></div>
-        <div></div>
       </section>
     </header>
     

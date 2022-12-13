cde0163685787a4a4a5c1b282880e28d4f227f6c
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2022-12-13 14:23:31 +0100

    started header rework

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import '../../styles/homepage/header.css'
-import Banner from './header-banner/Banner'
+import headerImage from '../../assets/img/header-image.jpg'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <h1 className=''>Frederik Skrubbeltrang</h1>
-      <Banner/>
-    </div>
+    <header className='header'>
+      <section className='upper-header'>
+        <h1 className=''>Frederik Skrubbeltrang</h1>
+        <div className='seperator-1'></div>
+      </section>
+      <section className='lower-header'>
+        <img src={headerImage} alt="header-image" />
+        <div className='seperator-2'></div>
+        <div></div>
+      </section>
+    </header>
     
   )
 }

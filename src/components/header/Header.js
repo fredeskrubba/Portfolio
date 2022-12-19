936d6d35e167b16e83d5afc75013bb3a21ec1103
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-12-19 10:43:42 +0100

    header finished

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,6 +9,23 @@ const Header = () => {
         <h1 className=''>Frederik Skrubbeltrang</h1>
         <div className='seperator-1'></div>
       </section>
+      <section className='lower-header'>
+        <article className='headings'>
+          <h2>UI/UX</h2>
+          <h2>Frontend</h2>
+          <h2>Backend</h2>
+        </article>
+        <figure>
+          <div className='bubble-cont-1'>
+            <div className='circle circle-med'></div>
+            <div className='circle circle-small'></div>
+          </div>
+          <div className='bubble-cont-2'>
+            <div className='circle circle-small'></div>
+            <div className='circle circle-big'></div>
+          </div>
+        </figure>
+      </section>
     </header>
     
   )

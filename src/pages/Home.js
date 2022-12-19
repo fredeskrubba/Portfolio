53ba2f744e8d94a8f54e42f8dc5bf16858b6472e
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-12-19 13:24:00 +0100

    burger menu made

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar/Navbar'
 import Header from '../components/header/Header'
 import About from '../components/About'
 import profile from '../assets/img/profile.jpg'
 import ProjectsSection from '../components/projects/ProjectsSection'
+import BurgerMenu from '../components/navbar/BurgerMenu'
+import { useState } from 'react'
 
 const Home = () => {
+  const [visible, setVisible] = useState('none')
+  
   return (
     <>
-      <Navbar/>
+      <Navbar setVisible={setVisible} visible={visible}/>
       <Header/>
       <About img={profile}/>
       <ProjectsSection/>
+      <BurgerMenu visible={visible}/>
     </>
   )
 }

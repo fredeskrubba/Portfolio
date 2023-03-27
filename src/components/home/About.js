a184420d9ced4b064a5be76003c8c5fbc4c7763a
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-03-27 14:40:26 +0200

    add new skills

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -4,6 +4,15 @@ import Skill from './skills/Skill'
 import {ReactComponent as FigmaLogo} from "../../assets/icons/figma-logo.svg"
 import {ReactComponent as PsLogo} from "../../assets/icons/ps-logo.svg"
 import {ReactComponent as AiLogo} from "../../assets/icons/ai-logo.svg"
+import {ReactComponent as HtmlLogo} from "../../assets/icons/html-icon.svg"
+import {ReactComponent as CssLogo} from "../../assets/icons/css-icon.svg"
+import {ReactComponent as JsLogo} from "../../assets/icons/js-icon.svg"
+import {ReactComponent as ReactLogo} from "../../assets/icons/react-icon.svg"
+import {ReactComponent as TailwindLogo} from "../../assets/icons/tailwind-icon.svg"
+import {ReactComponent as NpmLogo} from "../../assets/icons/npm-icon.svg"
+import {ReactComponent as NodeLogo} from "../../assets/icons/node-icon.svg"
+import {ReactComponent as SqlLogo} from "../../assets/icons/sql-icon.svg"
+import {ReactComponent as MongoLogo} from "../../assets/icons/mongo-icon.svg"
 import { useState } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -23,7 +32,46 @@ const About = ({img, alt}) => {
       logo: AiLogo
     },
   ]
-
+  const frontSkills = [
+    {
+      title: "HTML",
+      logo: HtmlLogo
+    },
+    {
+      title: "CSS/SCSS",
+      logo: CssLogo
+    },
+    {
+      title: "Javascript",
+      logo: JsLogo
+    },
+    {
+      title: "React",
+      logo: ReactLogo
+    },
+    {
+      title: "Tailwind.css",
+      logo: TailwindLogo
+    },
+    {
+      title: "NPM",
+      logo: NpmLogo
+    },
+  ]
+  const backSkills = [
+    {
+      title: "Node.js",
+      logo: NodeLogo
+    },
+    {
+      title: "MySql",
+      logo: SqlLogo
+    },
+    {
+      title: "MongoDB",
+      logo: MongoLogo
+    },
+  ]
   const [activeIndex, setActiveIndex] = useState(0);
   
   
@@ -37,17 +85,17 @@ const About = ({img, alt}) => {
           <p className='sub-text'>I et hav af programmeringsprog, design-programmer, og Javascript frameworks, har jeg tilegnet mig evner inden for en række teknologier som er populære, veldokumenterede og minimalistiske</p>
           <article className='carousel'>
             <section className='inner' style={{transform: `translateX(-${activeIndex * 100}%)`}}>
-              <div className="ui skill-container">
+              <div className="flex skill-container">
                 <h2>UI/UX</h2>
                 <Skill skills={uiSkills}/>
               </div>
-              <div className="ui skill-container">
+              <div className="flex skill-container">
                 <h2>Frontend</h2>
-                <Skill skills={uiSkills}/>
+                <Skill skills={frontSkills} grid={true}/>
               </div>
-              <div className="ui skill-container">
+              <div className="flex skill-container">
                 <h2>Backend</h2>
-                <Skill skills={uiSkills}/>
+                <Skill skills={backSkills}/>
               </div>
             </section>
           </article>

ce21c0adcc0aa1852c2ca520f5731589548a0fdb
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-03-27 12:56:31 +0200

    add new carousel library

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -4,6 +4,8 @@ import Skill from './skills/Skill'
 import {ReactComponent as FigmaLogo} from "../../assets/icons/figma-logo.svg"
 import {ReactComponent as PsLogo} from "../../assets/icons/ps-logo.svg"
 import {ReactComponent as AiLogo} from "../../assets/icons/ai-logo.svg"
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const About = ({img, alt}) => {
 
@@ -25,12 +27,20 @@ const About = ({img, alt}) => {
     <section className='about-section-desktop'>
           <h2>Mit Skillset</h2>
           <p className='sub-text'>I et hav af programmeringsprog, design-programmer, og Javascript frameworks, har jeg tilegnet mig evner inden for en række teknologier som er populære, veldokumenterede og minimalistiske</p>
-          <article className="skill-carousel">
+          <Carousel infiniteLoop={true}>
             <div className="ui skill-container">
               <h2>UI/UX</h2>
               <Skill skills={uiSkills}/>
             </div>
-          </article>
+            <div className="ui skill-container">
+              <h2>Frontend</h2>
+              <Skill skills={uiSkills}/>
+            </div>
+            <div className="ui skill-container">
+              <h2>Backend</h2>
+              <Skill skills={uiSkills}/>
+            </div>
+          </Carousel>
     </section>
   )
 }

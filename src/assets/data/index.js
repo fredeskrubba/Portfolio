ee69248a0815ff8f2c155c9c52df5505e92a3e13
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-04-09 13:23:36 +0200

    svg + data changes, info tabs fixed

---
+++ src/assets/data/index.js
@@ -0,0 +1,61 @@
+import Arrow from '../icons/Arrow.svg';
+import AzureDevops from '../icons/azure-devops.svg';
+import Blazor from '../icons/Blazor.svg';
+import CSharp from '../icons/CSharp.svg';
+import Css from '../icons/CSS.svg';
+import Darkmode from '../icons/Darkmode.svg';
+import Docker from '../icons/docker.svg';
+import EnglishLanguage from '../icons/English-language.svg';
+import EntityFramework from '../icons/entity-framework.svg';
+import ExperienceIcon from '../icons/experience-icon.svg';
+import Figma from '../icons/figma.svg';
+import Git from '../icons/Git.svg';
+import GithubIcon from '../icons/github-icon.svg';
+import Html from '../icons/HTML.svg';
+import Javascript from '../icons/Javascript.svg';
+import Lightmode from '../icons/Lightmode.svg';
+import LinkedinIcon from '../icons/linkedin-icon.svg';
+import MailIcon from '../icons/mail-icon.svg';
+import MySql from '../icons/MySQL.svg';
+import Net from '../icons/net.svg';
+import Nodejs from '../icons/Nodejs.svg';
+import Postgres from '../icons/Postgres.svg';
+import ProjectsIcon from '../icons/projects-icon.svg';
+import React from '../icons/React.svg';
+import RedirectArrow from '../icons/redirect-arrow.svg';
+import Redux from '../icons/Redux.svg';
+import SqlServer from '../icons/sql-server.svg';
+import StackIcon from '../icons/stack-icon.svg';
+import Tailwind from '../icons/Tailwind.svg';
+
+export {
+  Arrow,
+  AzureDevops,
+  Blazor,
+  CSharp,
+  Css,
+  Darkmode,
+  Docker,
+  EnglishLanguage,
+  EntityFramework,
+  ExperienceIcon,
+  Figma,
+  Git,
+  GithubIcon,
+  Html,
+  Javascript,
+  Lightmode,
+  LinkedinIcon,
+  MailIcon,
+  MySql,
+  Net,
+  Nodejs,
+  Postgres,
+  ProjectsIcon,
+  React,
+  RedirectArrow,
+  Redux,
+  SqlServer,
+  StackIcon,
+  Tailwind,
+};

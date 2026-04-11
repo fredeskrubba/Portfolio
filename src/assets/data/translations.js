66fab4fdabd4a54119e36ae25e3c9842c42bd180
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-04-11 16:03:26 +0200

    lang update

---
+++ src/assets/data/translations.js
@@ -0,0 +1,16 @@
+ const translations = {
+  en: {
+    title: "Welcome",
+    description: "I am a 22-year-old software developer based in Denmark. I have a passion for creating innovative and efficient software solutions that have a positive impact on people's lives. With a strong background in programming and problem-solving, I enjoy tackling complex challenges and continuously learning new technologies to stay at the forefront of the industry.",
+    cvButtonText: "Download my CV",
+    experienceHeader: "My Experience",
+  },
+  da: {
+    title: "Velkommen",
+    description: "Jeg er en 22-årig softwareudvikler baseret i Danmark. Jeg har en passion for at skabe innovative og effektive softwareløsninger, der har en positiv indvirkning på menneskers liv. Med en stærk baggrund i programmering og problemopløsning nyder jeg at tackle komplekse udfordringer og kontinuerligt lære nye teknologier for at blive ved de første i branchen.",
+    cvButtonText: "Download mit CV",
+    experienceHeader: "Min Erfaring",
+  }
+};  
+
+export { translations };
